eb81dc85d34c88e78b8290351b1045a47ab08eb5
Paul <pjavaloy@emeal.nttdata.com>
2022-12-01 22:18:32 +0100

    Rename giftWrap variable to wrapper

--- src/01_Automatizando_envolver_regalos_de_navidad/index.js
+++ src/01_Automatizando_envolver_regalos_de_navidad/index.js
@@ -4,8 +4,8 @@ export function wrapping(gifts) {
     const SEPARATOR = '\n'
 
     const giftWrapped = WRAP_SYMBOL + gift + WRAP_SYMBOL
-    const giftWrap = WRAP_SYMBOL.repeat(giftWrapped.length)
-    return [giftWrap, giftWrapped, giftWrap].join(SEPARATOR)
+    const wrapper = WRAP_SYMBOL.repeat(giftWrapped.length)
+    return [wrapper, giftWrapped, wrapper].join(SEPARATOR)
   }
 
   return gifts.map(wrap)
