e1b897ebacc85823fef0a508e60e1d4e30bd975e
Paul <pjavaloy@emeal.nttdata.com>
2022-12-02 18:33:31 +0100

    Fix semicolons

--- src/02_Nadie_quiere_hacer_horas_extra/index.js
+++ src/02_Nadie_quiere_hacer_horas_extra/index.js
@@ -1,15 +1,14 @@
 function countHours(year, holidays) {
-  const EXTRA_HOURS = 2;
+  const EXTRA_HOURS = 2
 
   const isWeekend = (date) => {
-    const SUNDAY = 0;
-    const SATUDARY = 6;
+    const SUNDAY = 0
+    const SATUDARY = 6
 
-    const dayOfWeek = date.getDay();
-    return dayOfWeek === SUNDAY || dayOfWeek === SATUDARY;
-  };
+    const dayOfWeek = date.getDay()
+    return dayOfWeek === SUNDAY || dayOfWeek === SATUDARY
+  }
+  const isWorkingHoliday = (date, year) => !isWeekend(date) && date.getFullYear() === year
 
-  const isWorkingHoliday = (date) => !isWeekend(date) && date.getFullYear() === year;
-
-  return holidays.filter((date) => isWorkingHoliday(new Date(`${year}-${date}`))).length * EXTRA_HOURS;
+  return holidays.filter((date) => isWorkingHoliday(new Date(`${year}-${date}`), year)).length * EXTRA_HOURS
 }
