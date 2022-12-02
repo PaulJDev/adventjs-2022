88452a0fb50d4f6efb4cb2c0da641414ab781958
Paul <pjavaloy@emeal.nttdata.com>
2022-12-02 17:48:49 +0100

    Add chanllege 01 more test

--- test/01.test.js
+++ test/01.test.js
@@ -1,19 +1,81 @@
-import test from 'node:test'
+import { describe, it } from 'node:test'
 import assert from 'node:assert'
 import { wrapping } from '../src/01_Automatizando_envolver_regalos_de_navidad/index.js'
 
-test('01 - Automatizando envolver regalos de navidad', () => {
-  // Arrange
-  const gifts = ['book', 'game', 'socks']
+describe('01 - Automatizando envolver regalos de navidad', () => {
+  it('should return an array', () => {
+    // Arrange
+    const gifts = ['regalo 1', 'regalo 2', 'regalo 3']
 
-  // Act
-  const wrappedGifts = wrapping(gifts)
+    // Act
+    const result = wrapping(gifts)
 
-  // Assert
-  const expected = [
-    '******\n*book*\n******',
-    '******\n*game*\n******',
-    '*******\n*socks*\n*******',
-  ]
-  assert.deepStrictEqual(wrappedGifts, expected)
+    // Assert
+    assert.strictEqual(Array.isArray(result), true)
+  })
+
+  it('should return gifts wrapped', () => {
+    // Arrange
+    const gifts = ['book', 'game', 'socks']
+
+    // Act
+    const wrappedGifts = wrapping(gifts)
+
+    // Assert
+    const expected = [
+      '******\n*book*\n******',
+      '******\n*game*\n******',
+      '*******\n*socks*\n*******',
+    ]
+    assert.deepStrictEqual(wrappedGifts, expected)
+  })
+
+  it('should return empty array when gifts is empty', () => {
+    // Arrange
+    const gifts = []
+
+    // Act
+    const wrappedGifts = wrapping(gifts)
+
+    // Assert
+    const expected = []
+    assert.deepStrictEqual(wrappedGifts, expected)
+  })
+
+  it('should return midu wrapped', () => {
+    // Arrange
+    const gifts = ['midu']
+
+    // Act
+    const wrappedGifts = wrapping(gifts)
+
+    // Assert
+    const expected = ['******\n*midu*\n******']
+    assert.deepStrictEqual(wrappedGifts, expected)
+  })
+
+  it('should return "a" wrapped', () => {
+    // Arrange
+    const gifts = ['a']
+
+    // Act
+    const wrappedGifts = wrapping(gifts)
+
+    // Assert
+    const expected = ['***\n*a*\n***']
+
+    assert.deepStrictEqual(wrappedGifts, expected)
+  })
+
+  it('wrapping an empty array should return an empty array', () => {
+    // Arrange
+    const gifts = []
+
+    // Act
+    const wrappedGifts = wrapping(gifts)
+
+    // Assert
+    const expected = []
+    assert.deepStrictEqual(wrappedGifts, expected)
+  })
 })
